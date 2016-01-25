aae7923a02e5f874a15a5a60094412ae6782dbca
zhouyong10 <1059981087@qq.com>
2016-01-25 07:42:55 -0800

    添加论坛流量任务和视频流量任务页面

--- backstage/router/flow.js
+++ backstage/router/flow.js
@@ -8,5 +8,12 @@ router.get('/taskHistory', function (req, res) {
     res.render('flowTaskHistory', {title: '流量业务任务历史', money: 10.01})
 });
 
+router.get('/forumTask', function (req, res) {
+    res.render('forumFlowTask', {title: '论坛流量任务', money: 10.01})
+});
+
+router.get('/videoTask', function (req, res) {
+    res.render('videoFlowTask', {title: '视频流量任务', money: 22});
+});
 
 module.exports = router;
