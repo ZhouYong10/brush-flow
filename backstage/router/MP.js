788fbec70c1fa47e73b911335bb0a9c01bf39ea8
zhouyong10 <1059981087@qq.com>
2016-01-26 02:08:14 -0800

    添加美拍业务表单模板

--- backstage/router/MP.js
+++ backstage/router/MP.js
@@ -8,16 +8,31 @@ router.get('/like', function (req, res) {
     res.render('MPlike', {title: '美拍点赞任务', money: 10.01})
 });
 
+router.get('/like/add', function (req, res) {
+    res.render('MPlikeAdd', {title: '添加美拍点赞任务', money: 10.01})
+});
+
 router.get('/comment', function (req, res) {
     res.render('MPcomment', {title: '美拍评论任务', money: 111});
+});
+
+router.get('/comment/add', function (req, res) {
+    res.render('MPcommentAdd', {title: '添加美拍评论任务', money: 111});
 });
 
 router.get('/attention', function (req, res) {
     res.render('MPattention', {title: '美拍关注任务', money: 22});
 });
 
+router.get('/attention/add', function (req, res) {
+    res.render('MPattentionAdd', {title: '添加美拍关注任务', money: 22});
+});
+
 router.get('/forward', function (req, res) {
     res.render('MPforward', {title: '美拍转发任务', money: 33});
 });
 
+router.get('/forward/add', function (req, res) {
+    res.render('MPforwardAdd', {title: '添加美拍转发任务', money: 33});
+});
 module.exports = router;
