a8fae4de1e14d717b1293713999a2b04657f0611
zhouyong10 <1059981087@qq.com>
2016-02-26 12:21:06 +0800

    添加Recharge类

---
+++ backstage/models/Recharge.js
@@ -0,0 +1,23 @@
+/**
+ * Created by zhouyong10 on 2/3/16.
+ */
+var db = require('../dbWrap');
+var Class = require('./Class');
+
+
+
+var Placard = new Class();
+
+
+Placard.extend(db);
+
+Placard.open = function() {
+    return Placard.openCollection('Placard');
+};
+
+Placard.include({
+
+});
+
+
+module.exports = Placard;
