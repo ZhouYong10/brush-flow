d0ace1161ef4b46776a56dbe3a84fd8459e3d250
zhouyong10 <1059981087@qq.com>
2016-03-03 16:00:01 +0800

    通过后台数据动态更新导航栏更新信息的显示

--- web-workflow/src/components/adminMenuContent/adminMenuContent.js
+++ web-workflow/src/components/adminMenuContent/adminMenuContent.js
@@ -3,15 +3,22 @@
  */
 $(function () {
     $.get('/admin/update/header/nav', function (obj) {
-        console.log(obj,'=========================');
+        updateNav(obj);
     });
 
-
     var socket = io();
-
-    //console.log(socket, '===============');
+    socket.on('updateNav', function(obj) {
+        updateNav(obj);
+    })
 });
 
 function updateNav(obj) {
-
+    for(var key in obj) {
+        if(obj[key] > 0) {
+            $('.tips.' + key).css('display', 'inline');
+            $('.updateNum.' + key).text(' ( ' + obj[key] + ' ) ');
+        }else {
+            $('.tips.' + key).css('display', 'none');
+        }
+    }
 }
