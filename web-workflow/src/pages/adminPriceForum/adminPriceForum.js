29c2ba35ce5413041b569925c2d7df867c51b3fb
zhouyong10 <1059981087@qq.com>
2016-03-05 12:33:25 +0800

    添加论坛价格项

--- web-workflow/src/pages/adminPriceForum/adminPriceForum.js
+++ web-workflow/src/pages/adminPriceForum/adminPriceForum.js
@@ -1,29 +1,27 @@
 /**
  * Created by ubuntu64 on 3/5/16.
  */
-var wxSmallType = '<td> ' +
+var smallType = '<td> ' +
     '<select class="am-form-group am-form-select smallType"> ' +
-    '<option value="article">原文</option> ' +
-    '<option value="like">阅读点赞</option> ' +
-    '<option value="reply">公众粉丝回复</option> ' +
-    '<option value="friend">个人好友</option> ' +
-    '<option value="code">好友地区扫码</option> ' +
+    '<option value="video">视频站点</option> ' +
+    '<option value="complex">综合社区</option> ' +
+    '<option value="female">女性时尚</option> ' +
+    '<option value="car">汽车社区</option> ' +
+    '<option value="estate">房产社区</option> ' +
+    '<option value="IT">IT科技类</option> ' +
+    '<option value="blog">博客类</option> ' +
+    '<option value="mother">母婴社区</option> ' +
+    '<option value="news">新闻门户</option> ' +
+    '<option value="travel">旅游社区</option> ' +
+    '<option value="economic">财经社区</option> ' +
+    '<option value="games">游戏社区</option> ' +
     '</select> ' +
     '</td> ';
-var wbSmallType = '<td> ' +
-    '<select class="am-form-group am-form-select smallType"> ' +
-    '<option value="like">高级赞</option> ' +
-    '<option value="vote">投票</option> ' +
-    '<option value="fens">顶级粉</option> ' +
-    '<option value="forward">转发</option> ' +
-    '</select> ' +
-    '</td> ';
-var mpSmallType = '<td> ' +
-    '<select class="am-form-group am-form-select smallType"> ' +
-    '<option value="like">点赞</option> ' +
-    '<option value="comment">评论</option> ' +
-    '<option value="attention">关注</option> ' +
-    '<option value="forward">转发</option> ' +
+var status = '<td> ' +
+    '<select class="am-form-group am-form-select status"> ' +
+    '<option value="normal">正常</option> ' +
+    '<option value="develop">开发中</option> ' +
+    '<option value="maintain">维护中</option> ' +
     '</select> ' +
     '</td> ';
 var saveBtn = '<button type="button" class="am-btn am-btn-primary am-radius am-btn-xs save">保存</button> ';
@@ -34,19 +32,16 @@ var changeBtn = '<button type="button" class="am-btn am-btn-primary am-radius am
 var deleteBtn = '<button type="button" class="am-btn am-btn-primary am-radius am-btn-xs delete">删除</button> ';
 var priceItem = '<tr> ' +
     '<td class="num"></td> ' +
-    '<td> ' +
-    '<select class="am-form-group am-form-select type"> ' +
-    '<option value="wx">微信</option> ' +
-    '<option value="mp">美拍</option> ' +
-    '<option value="wb">微博</option> ' +
-    '</select> ' +
-    '</td> ' +
-    wxSmallType+
-    '<td> <input class="am-form-field am-input-sm name" type="text" placeholder="名称"> </td> ' +
+    smallType+
+    '<td> <button type="button" class="am-btn am-btn-primary am-radius am-btn-xs upload">上传</button> </td> ' +
+    '<td> <input class="am-form-field am-input-sm name" type="text" placeholder="站点名称"> </td> ' +
+    '<td> <input class="am-form-field am-input-sm name" type="text" placeholder="站点网址"> </td> ' +
     '<td> <input class="am-form-field am-input-sm adminPrice" type="text" placeholder="管理员价格"> </td> ' +
     '<td> <input class="am-form-field am-input-sm topPrice" type="text" placeholder="顶级代理价格"> </td> ' +
     '<td> <input class="am-form-field am-input-sm superPrice" type="text" placeholder="超级代理价格"> </td> ' +
     '<td> <input class="am-form-field am-input-sm goldPrice" type="text" placeholder="金牌代理价格"> </td> ' +
+    status +
+    '<td> <textarea class="remark" rows="1" cols="28"></textarea> </td> ' +
     '<td> ' +
     saveBtn +
     cancelBtn +
