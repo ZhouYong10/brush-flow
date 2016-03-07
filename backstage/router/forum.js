60331a31b4798dae2983ac9f684f17d1aa852e32
zhouyong10 <1059981087@qq.com>
2016-03-07 16:39:29 +0800

    按分类显示论坛业务

--- backstage/router/forum.js
+++ backstage/router/forum.js
@@ -8,53 +8,121 @@ var Product = require('../models/Product');
 
 var router = require('express').Router();
 
-router.get('/video', function (req, res) {
+function viewForumItem(smallType, res, obj) {
+    Product.open().find({type: 'forum', smallType: smallType})
+        .then(function (products) {
+            obj.products = products;
+            res.render('forumTemplate', obj);
+        });
+}
 
-    res.render('forumTemplate', {title: '视频站点', money: req.session.funds})
+
+router.get('/video', function (req, res) {
+    var user = req.session.user;
+    viewForumItem('video', res, {
+        title: '视频站点',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/complex', function (req, res) {
-    res.render('forumTemplate', {title: '综合社区', money: req.session.funds})
+    var user = req.session.user;
+    viewForumItem('complex', res, {
+        title: '综合社区',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/female', function (req, res) {
-    res.render('forumTemplate', {title: '女性时尚', money: req.session.funds})
+    var user = req.session.user;
+    viewForumItem('female', res, {
+        title: '女性时尚',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/car', function (req, res) {
-    res.render('forumTemplate', {title: '汽车社区', money: req.session.funds})
+    var user = req.session.user;
+    viewForumItem('car', res, {
+        title: '汽车社区',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/estate', function (req, res) {
-    res.render('forumTemplate', {title: '房产社区', money: req.session.funds})
+    var user = req.session.user;
+    viewForumItem('estate', res, {
+        title: '房产社区',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/IT', function (req, res) {
-    res.render('forumTemplate', {title: 'IT科技类', money: req.session.funds})
+    var user = req.session.user;
+    viewForumItem('IT', res, {
+        title: 'IT科技类',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/blog', function (req, res) {
-    res.render('forumTemplate', {title: '博客类', money: req.session.funds})
+    var user = req.session.user;
+    viewForumItem('blog', res, {
+        title: '博客类',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/child', function (req, res) {
-    res.render('forumTemplate', {title: '母婴社区', money: req.session.funds})
+    var user = req.session.user;
+    viewForumItem('mother', res, {
+        title: '母婴社区',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/news', function (req, res) {
-    res.render('forumTemplate', {title: '新闻门户', money: req.session.funds})
+    var user = req.session.user;
+    viewForumItem('news', res, {
+        title: '新闻门户',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/travel', function (req, res) {
-    res.render('forumTemplate', {title: '旅游社区', money: req.session.funds})
+    var user = req.session.user;
+    viewForumItem('travel', res, {
+        title: '旅游社区',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/finance', function (req, res) {
-    res.render('forumTemplate', {title: '财经社区', money: req.session.funds})
+    var user = req.session.user;
+    viewForumItem('economic', res, {
+        title: '财经社区',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/games', function (req, res) {
-    res.render('forumTemplate', {title: '游戏社区', money: req.session.funds})
+    var user = req.session.user;
+    viewForumItem('games', res, {
+        title: '游戏社区',
+        money: req.session.funds,
+        userRole: user.role
+    });
 });
 
 router.get('/taskHistory', function (req, res) {
