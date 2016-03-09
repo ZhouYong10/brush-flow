8148e451c0007afd3d885ac044f794f554312924
zhouyong10 <1059981087@qq.com>
2016-03-09 12:12:33 +0800

    修复后台导航栏微信任务处理状态更新不显示问题

--- web-workflow/src/components/adminMenuContent/adminMenuContent.js
+++ web-workflow/src/components/adminMenuContent/adminMenuContent.js
@@ -24,4 +24,14 @@ function updateNav(obj, isAdd) {
         }
         $('.updateNum.' + key).text(' ( ' + obj[key] + ' ) ');
     }
+
+    var wxArticle = parseInt($('.updateNum.wxArticle').text().split(' ')[2]);
+    var wxLike = parseInt($('.updateNum.wxLike').text().split(' ')[2]);
+    var wxReply = parseInt($('.updateNum.wxReply').text().split(' ')[2]);
+    var wxFriend = parseInt($('.updateNum.wxFriend').text().split(' ')[2]);
+    var wxCode = parseInt($('.updateNum.wxCode').text().split(' ')[2]);
+    var wxNum = wxArticle + wxLike + wxReply + wxFriend + wxCode;
+    if(wxNum > 0) {
+        $('.tips.wxArticle.wxLike.wxReply.wxFriend.wxCode').css('display', 'inline');
+    }
 }
