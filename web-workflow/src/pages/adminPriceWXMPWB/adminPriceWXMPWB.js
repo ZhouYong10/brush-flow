939bc4a87e6bd1098275d26e71d150e9d412925d
zhouyong10 <1059981087@qq.com>
2016-03-10 18:28:53 +0800

    修改添加微信价格项小类

--- web-workflow/src/pages/adminPriceWXMPWB/adminPriceWXMPWB.js
+++ web-workflow/src/pages/adminPriceWXMPWB/adminPriceWXMPWB.js
@@ -3,11 +3,14 @@
  */
 var wxSmallType = '<td> ' +
     '<select class="am-form-group am-form-select smallType"> ' +
-    '<option value="article">原文</option> ' +
-    '<option value="like">阅读点赞</option> ' +
-    '<option value="fans">公众粉丝</option> ' +
-    '<option value="reply">公众粉丝回复</option> ' +
     '<option value="friend">个人好友</option> ' +
+    '<option value="fans">公众粉丝</option> ' +
+    '<option value="fansReply">公众粉丝回复</option> ' +
+    '<option value="article">原文</option> ' +
+    '<option value="share">分享</option> ' +
+    '<option value="collect">收藏</option> ' +
+    '<option value="read">阅读</option> ' +
+    '<option value="like">点赞</option> ' +
     '<option value="code">好友地区扫码</option> ' +
     '</select> ' +
     '</td> ';
