2b10d1bf6ff6efd2ee5a219723712e6427f0c5fc
zhouyong10 <1059981087@qq.com>
2016-03-15 15:43:31 +0800

    添加微信阅读页面的前端验证

---
+++ web-workflow/src/pages/WXlikeAdd/WXlikeAdd.js
@@ -0,0 +1,50 @@
+/**
+ * Created by ubuntu64 on 3/15/16.
+ */
+var Vue = require('vue');
+Vue.use(require('vue-validator'));
+Vue.use(require('vue-resource'));
+
+var Utils = require('utils');
+
+new Vue({
+    el: '#wxLike',
+    data: {
+        price: '',
+        price2: '',
+        address: '',
+        title: '',
+        articleTitle: '',
+        totalPrice: 0,
+        num: '',
+        num2: '',
+        funds: 0
+    },
+    methods: {
+        parseAddress: function() {
+            var self = this;
+            Utils.parseAddress(self.$http, self.address)
+                .then(function(title) {
+                    self.title = title;
+                },function(message) {
+                    alert(message);
+                })
+        },
+        total: function() {
+            this.totalPrice = (this.price * this.num + this.price2 * this.num2).toFixed(4);
+        }
+    },
+    validators: {
+        isaddress: function(val) {
+            return /((http|ftp|https|file):\/\/([\w\-]+\.)+[\w\-]+(\/[\w\u4e00-\u9fa5\-\.\/?\@\%\!\&=\+\~\:\#\;\,]*)?)/ig.test(val);
+        },
+        minnum: function(val) {
+            return val <= this.num;
+        },
+        isnum: Utils.isNum,
+        min30: Utils.min30,
+        maxprice: function() {
+            return this.price * this.num + this.price2 * this.num2 <= this.funds;
+        }
+    }
+});
