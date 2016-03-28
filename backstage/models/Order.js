f20c20fa2aeaf49d55dca5203c369ff92b019795
zhouyong10 <1059981087@qq.com>
2016-03-28 12:51:47 +0800

    处理后订单直接退款也标示为处理

--- backstage/models/Order.js
+++ backstage/models/Order.js
@@ -27,7 +27,7 @@ Order.extend({
             var order = orders[i];
             if(order.status == '已处理'){
                 var dealTime = order.dealTime, num = order.num,
-                    delay = 3 * 60 * 1000, speed = order.speed ? order.speed : speedNum;
+                    delay = 0 * 60 * 1000, speed = order.speed ? order.speed : speedNum;
                 var allTimes = (parseInt(num / speed) + ((num % speed == 0) ? 0 : 1)) * 60 * 1000;
                 var currentTimes = new Date().getTime() - new Date(dealTime).getTime() - delay;
 
@@ -253,6 +253,7 @@ Order.include({
     refund: function(info, callback) {
         var self = this;
         self.status = '已退款';
+        self.error = '已处理';
         self.refundInfo = info;
         Order.open().updateById(self._id, self)
             .then(function () {
