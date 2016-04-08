171449df6b7733c2ed9516bd5dc31bb4115295b7
zhouyong10 <1059981087@qq.com>
2016-04-08 11:47:00 +0800

    停止转单

--- backstage/models/Order.js
+++ backstage/models/Order.js
@@ -380,113 +380,113 @@ Order.include({
 /*
 * wx read and like
 * */
-var post_key = '';
-var task_time = 1000 * 20;
-var valIndex = startInterval();
-
-function startInterval() {
-    var random = (Math.random() / 3 * 100).toFixed(0);
-    task_time = random >= 5 ? random * 1000 : 5 * 1000;
-    return setInterval(function() {
-        if(post_key == ''){
-            noKey(clearTime);
-        }else {
-            yesKey(clearTime);
-        }
-    }, task_time);
-}
-
-function clearTime() {
-    clearInterval(valIndex);
-    valIndex = startInterval();
-}
-
-function noKey(callback) {
-    request.get({
-        url:'http://120.25.203.122/tuike_sys.php',
-        headers:{
-            "Accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            "Accept-Encoding": 'gzip, deflate, sdch',
-            "Accept-Language": 'zh-CN,zh;q=0.8',
-            "Cache-Control": 'max-age=0',
-            "Connection": 'keep-alive',
-            "Cookie": 'PHPSESSID=rrk21m5gqgbdmbd3lsjs312o10',
-            "Host": '120.25.203.122',
-            "Referer": 'http://120.25.203.122/login.html',
-            "Upgrade-Insecure-Requests": 1,
-            "User-Agent": 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
-        }
-    },function(err,res,body){
-        if(err) {
-            return console.log(err);
-        }
-        var $ = cheerio.load(body);
-        post_key = $('#post_key').val();
-        yesKey(callback);
-    });
-}
-
-function yesKey(callback) {
-    Order.open().findOne({
-        status: '未处理',
-        type: 'wx',
-        smallType: {$in: ['read', 'like']}
-    }).then(function (result) {
-        if (result) {
-            request.post({
-                url:'http://120.25.203.122/tuike_sys.php',
-                headers:{
-                    "Accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                    "Accept-Encoding": 'gzip, deflate, sdch',
-                    "Accept-Language": 'zh-CN,zh;q=0.8',
-                    "Cache-Control": 'max-age=0',
-                    "Connection": 'keep-alive',
-                    "Cookie": 'PHPSESSID=rrk21m5gqgbdmbd3lsjs312o10',
-                    "Host": '120.25.203.122',
-                    "Referer": 'http://120.25.203.122/tuike_sys.php',
-                    "Upgrade-Insecure-Requests": 1,
-                    "User-Agent": 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
-                },
-                formData: {
-                    url: result.address,
-                    speed: result.speed ? result.speed : 167,
-                    read_cnt: result.num,
-                    post_key: post_key,
-                    like_cnt: result.num2 ? result.num2 : 0,
-                    like: (result.num2 / result.num).toFixed(3)
-                }
-            },function(err,res,body){
-                if(err) {
-                    return console.log(err);
-                }
-                var $ = cheerio.load(body);
-                post_key = $('#post_key').val();
-                var resultInstance = Order.wrapToInstance(result);
-                resultInstance.complete(function () {
-                    console.log('自动处理订单完成了, href = ' + result.address);
-                    callback();
-                });
-
-                //$('tbody a').each(function (i, e) {
-                //    var taskHref = $(e).attr('href');
-                //    if(taskHref == result.address) {
-                //        var resultInstance = Order.wrapToInstance(result);
-                //        resultInstance.complete(function() {
-                //            console.log('自动处理订单完成了, href = ' + result.address);
-                //            callback();
-                //        })
-                //    }else {
-                //        post_key = '';
-                //        callback();
-                //    }
-                //});
-            });
-        }else {
-            post_key = '';
-            callback();
-        }
-    });
-}
+//var post_key = '';
+//var task_time = 1000 * 20;
+//var valIndex = startInterval();
+//
+//function startInterval() {
+//    var random = (Math.random() / 3 * 100).toFixed(0);
+//    task_time = random >= 5 ? random * 1000 : 5 * 1000;
+//    return setInterval(function() {
+//        if(post_key == ''){
+//            noKey(clearTime);
+//        }else {
+//            yesKey(clearTime);
+//        }
+//    }, task_time);
+//}
+//
+//function clearTime() {
+//    clearInterval(valIndex);
+//    valIndex = startInterval();
+//}
+//
+//function noKey(callback) {
+//    request.get({
+//        url:'http://120.25.203.122/tuike_sys.php',
+//        headers:{
+//            "Accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+//            "Accept-Encoding": 'gzip, deflate, sdch',
+//            "Accept-Language": 'zh-CN,zh;q=0.8',
+//            "Cache-Control": 'max-age=0',
+//            "Connection": 'keep-alive',
+//            "Cookie": 'PHPSESSID=rrk21m5gqgbdmbd3lsjs312o10',
+//            "Host": '120.25.203.122',
+//            "Referer": 'http://120.25.203.122/login.html',
+//            "Upgrade-Insecure-Requests": 1,
+//            "User-Agent": 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+//        }
+//    },function(err,res,body){
+//        if(err) {
+//            return console.log(err);
+//        }
+//        var $ = cheerio.load(body);
+//        post_key = $('#post_key').val();
+//        yesKey(callback);
+//    });
+//}
+//
+//function yesKey(callback) {
+//    Order.open().findOne({
+//        status: '未处理',
+//        type: 'wx',
+//        smallType: {$in: ['read', 'like']}
+//    }).then(function (result) {
+//        if (result) {
+//            request.post({
+//                url:'http://120.25.203.122/tuike_sys.php',
+//                headers:{
+//                    "Accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+//                    "Accept-Encoding": 'gzip, deflate, sdch',
+//                    "Accept-Language": 'zh-CN,zh;q=0.8',
+//                    "Cache-Control": 'max-age=0',
+//                    "Connection": 'keep-alive',
+//                    "Cookie": 'PHPSESSID=rrk21m5gqgbdmbd3lsjs312o10',
+//                    "Host": '120.25.203.122',
+//                    "Referer": 'http://120.25.203.122/tuike_sys.php',
+//                    "Upgrade-Insecure-Requests": 1,
+//                    "User-Agent": 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+//                },
+//                formData: {
+//                    url: result.address,
+//                    speed: result.speed ? result.speed : 167,
+//                    read_cnt: result.num,
+//                    post_key: post_key,
+//                    like_cnt: result.num2 ? result.num2 : 0,
+//                    like: (result.num2 / result.num).toFixed(3)
+//                }
+//            },function(err,res,body){
+//                if(err) {
+//                    return console.log(err);
+//                }
+//                var $ = cheerio.load(body);
+//                post_key = $('#post_key').val();
+//                var resultInstance = Order.wrapToInstance(result);
+//                resultInstance.complete(function () {
+//                    console.log('自动处理订单完成了, href = ' + result.address);
+//                    callback();
+//                });
+//
+//                //$('tbody a').each(function (i, e) {
+//                //    var taskHref = $(e).attr('href');
+//                //    if(taskHref == result.address) {
+//                //        var resultInstance = Order.wrapToInstance(result);
+//                //        resultInstance.complete(function() {
+//                //            console.log('自动处理订单完成了, href = ' + result.address);
+//                //            callback();
+//                //        })
+//                //    }else {
+//                //        post_key = '';
+//                //        callback();
+//                //    }
+//                //});
+//            });
+//        }else {
+//            post_key = '';
+//            callback();
+//        }
+//    });
+//}
 
 
 module.exports = Order;
