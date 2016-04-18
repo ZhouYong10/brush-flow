27bedbd9ea5959309a2e674fb2635ae3639c0e18
zhouyong10 <1059981087@qq.com>
2016-04-18 16:59:34 +0800

    客户端提交视频流量订单

--- backstage/router/flow.js
+++ backstage/router/flow.js
@@ -71,4 +71,52 @@ router.get('/videoTask', function (req, res) {
         });
 });
 
+router.post('/videoTask', function (req, res) {
+    var orderInfo = req.body;
+    if(orderInfo.num <= 10000) {
+        orderInfo.num = 10;
+    }else {
+        orderInfo.num = parseInt(orderInfo.num / 1000) + (orderInfo.num % 1000 > 0 ? 1 : 0);
+    }
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            var order = Order.wrapToInstance(orderInfo);
+            order.createAndSave(user, {type: 'flow', smallType: 'video', name: orderInfo.name})
+                .then(function () {
+                    socketIO.emit('updateNav', {'flow': 1});
+                    res.redirect('/flow/taskHistory');
+                }, function() {
+                    res.send('<h1>您的余额不足，请充值！ 顺便多说一句，请不要跳过页面非法提交数据。。。不要以为我不知道哦！！</h1>')
+                });
+        });
+});
+
+router.get('/video/get/price', function (req, res) {
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            var address = req.query.address;
+            var arr = address.split('/')[2].split('.');
+            arr.shift();
+            var mainUrl = arr.join('.');
+            Product.open().findOne({
+                type: 'flow',
+                address: new RegExp(mainUrl)
+            }).then(function (result) {
+                if(result) {
+                    var instance = Product.wrapToInstance(result);
+                    res.send({
+                        isOk: true,
+                        price: instance.getPriceByRole(user.role),
+                        name: result.name
+                    });
+                }else {
+                    res.send({
+                        isOk: false,
+                        message: '对不起，暂时不支持该链接地址！'
+                    })
+                }
+            });
+        });
+});
+
 module.exports = router;
