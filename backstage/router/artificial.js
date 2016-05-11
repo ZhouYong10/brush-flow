78ccde0db2b2acfb31592cf7885b9b2357329beb
zhouyong10 <1059981087@qq.com>
2016-05-11 16:27:09 +0800

    添加客户端导航栏中人工平台选项中的所有路由和页面

--- backstage/router/artificial.js
+++ backstage/router/artificial.js
@@ -1,9 +1,130 @@
 /**
  * Created by ubuntu64 on 5/11/16.
  */
+var User = require('../models/User');
+var Order = require('../models/Order');
 
 
 var router = require('express').Router();
 
+router.get('/WX/fans', function (req, res) {
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            Order.open().findPages({
+                    userId: user._id,
+                    type: 'wx',
+                    smallType: 'friend'
+                }, (req.query.page ? req.query.page : 1))
+                .then(function(obj) {
+                    Order.addSchedule(obj.results, 10);
+                    res.render('handleWXfans', {
+                        title: '人工微信粉丝(回复)',
+                        money: user.funds,
+                        username: user.username,
+                        userStatus: user.status,
+                        role: user.role,
+                        orders: obj.results,
+                        pages: obj.pages,
+                        path: '/WX/friend'
+                    })
+                })
+        });
+});
+
+router.get('/WX/friend', function (req, res) {
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            Order.open().findPages({
+                    userId: user._id,
+                    type: 'wx',
+                    smallType: 'friend'
+                }, (req.query.page ? req.query.page : 1))
+                .then(function(obj) {
+                    Order.addSchedule(obj.results, 10);
+                    res.render('handleWXfriend', {
+                        title: '人工微信个人好友',
+                        money: user.funds,
+                        username: user.username,
+                        userStatus: user.status,
+                        role: user.role,
+                        orders: obj.results,
+                        pages: obj.pages,
+                        path: '/WX/friend'
+                    })
+                })
+        });
+});
+
+router.get('/WX/vote', function (req, res) {
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            Order.open().findPages({
+                    userId: user._id,
+                    type: 'wx',
+                    smallType: 'friend'
+                }, (req.query.page ? req.query.page : 1))
+                .then(function(obj) {
+                    Order.addSchedule(obj.results, 10);
+                    res.render('handleWXvote', {
+                        title: '人工微信投票',
+                        money: user.funds,
+                        username: user.username,
+                        userStatus: user.status,
+                        role: user.role,
+                        orders: obj.results,
+                        pages: obj.pages,
+                        path: '/WX/friend'
+                    })
+                })
+        });
+});
+
+router.get('/WX/code', function (req, res) {
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            Order.open().findPages({
+                    userId: user._id,
+                    type: 'wx',
+                    smallType: 'friend'
+                }, (req.query.page ? req.query.page : 1))
+                .then(function(obj) {
+                    Order.addSchedule(obj.results, 10);
+                    res.render('handleWXcode', {
+                        title: '人工微信扫码',
+                        money: user.funds,
+                        username: user.username,
+                        userStatus: user.status,
+                        role: user.role,
+                        orders: obj.results,
+                        pages: obj.pages,
+                        path: '/WX/friend'
+                    })
+                })
+        });
+});
+
+router.get('/WX/article', function (req, res) {
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            Order.open().findPages({
+                    userId: user._id,
+                    type: 'wx',
+                    smallType: 'read'
+                }, (req.query.page ? req.query.page : 1))
+                .then(function (obj) {
+                    Order.addSchedule(obj.results, 50);
+                    res.render('handleWXarticle', {
+                        title: '人工微信原文/收藏/分享',
+                        money: user.funds,
+                        username: user.username,
+                        userStatus: user.status,
+                        role: user.role,
+                        orders: obj.results,
+                        pages: obj.pages,
+                        path: '/WX/like'
+                    })
+                });
+        });
+});
 
 module.exports = router;
