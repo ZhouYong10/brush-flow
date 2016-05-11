1101e56b2933ab28eca64176b192d246943d1cd4
zhouyong10 <1059981087@qq.com>
2016-05-11 15:40:24 +0800

    添加任务大厅路由和页面

--- backstage/router/task.js
+++ backstage/router/task.js
@@ -1,9 +1,33 @@
 /**
  * Created by ubuntu64 on 5/11/16.
  */
-
+var User = require('../models/User');
+var Order = require('../models/Order');
 
 var router = require('express').Router();
 
+
+router.get('/all', function (req, res) {
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            Order.open().findPages({
+                    userId: user._id,
+                    type: 'forum'
+                }, (req.query.page ? req.query.page : 1))
+                .then(function (obj) {
+                    Order.addSchedule(obj.results, 1);
+                    res.render('handleTaskAll', {
+                        title: '任务大厅',
+                        money: user.funds,
+                        role: user.role,
+                        userStatus: user.status,
+                        username: user.username,
+                        orders: obj.results,
+                        pages: obj.pages,
+                        path: '/forum/taskHistory'
+                    });
+                });
+        });
+});
 
 module.exports = router;
