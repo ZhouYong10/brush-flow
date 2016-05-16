6f5182505fa68afd3b184910e81e0f070ab16dd4
zhouyong10 <1059981087@qq.com>
2016-05-16 15:27:18 +0800

    图片预览

--- web-workflow/src/pages/handleWXfansAdd/handleWXfansAdd.js
+++ web-workflow/src/pages/handleWXfansAdd/handleWXfansAdd.js
@@ -25,6 +25,42 @@ new Vue({
             }else {
                 this.count = (parseFloat(this.myFansPrice) * parseInt(this.num)).toFixed(4);
             }
+        },
+        viewImg: function() {
+            var $file = $('#doc-ipt-file-2');
+            var fileObj = $file[0];
+            var windowURL = window.URL || window.webkitURL;
+            var dataURL;
+            var $img = $("#preview");
+
+            if(fileObj && fileObj.files && fileObj.files[0]){
+                dataURL = windowURL.createObjectURL(fileObj.files[0]);
+                $img.attr('src',dataURL);
+            }else{
+                dataURL = $file.val();
+
+                // $img.css("filter",'progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod = scale,src="' + dataURL + '")');
+
+                // var imgObj = document.getElementById("preview");
+                // imgObj.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale,src=\"" + dataURL + "\")";
+                // imgObj.style.width = "48px";
+                // imgObj.style.height = "48px";
+
+                var imgObj = document.getElementById("preview");
+                // 两个坑:
+                // 1、在设置filter属性时，元素必须已经存在在DOM树中，动态创建的Node，也需要在设置属性前加入到DOM中，先设置属性在加入，无效；
+                // 2、src属性需要像下面的方式添加，上面的两种方式添加，无效；
+                imgObj.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)";
+                imgObj.filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = dataURL;
+
+            }
+        },
+        check: function(e) {
+            if(!$('#doc-ipt-file-2').val()){
+                e.stopPropagation();
+                e.preventDefault();
+                $('#photoTip').css('display','inline-block')
+            }
         }
     },
     validators: {
