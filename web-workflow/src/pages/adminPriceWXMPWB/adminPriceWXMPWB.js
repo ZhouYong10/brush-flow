30788d6c7a16e4da446b717e0eb2147e7a420cb4
zhouyong10 <1059981087@qq.com>
2016-09-19 15:32:22 +0800

    后台价格添加微信图文评论

--- web-workflow/src/pages/adminPriceWXMPWB/adminPriceWXMPWB.js
+++ web-workflow/src/pages/adminPriceWXMPWB/adminPriceWXMPWB.js
@@ -11,6 +11,7 @@ var wxSmallType = '<td> ' +
     '<option value="collect">收藏</option> ' +
     '<option value="read">阅读</option> ' +
     '<option value="like">点赞</option> ' +
+    '<option value="comment">图文评论</option> ' +
     '<option value="code">扫码关注</option> ' +
     '<option value="codeReply">扫码回复</option> ' +
     '</select> ' +
