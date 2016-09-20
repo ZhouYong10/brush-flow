c71da8a6a9120106775aa81009a92fbaf0a21f1c
zhouyong10 <1059981087@qq.com>
2016-09-20 17:07:36 +0800

    前端将视频流量任务展开成列表

--- backstage/router/flow.js
+++ backstage/router/flow.js
@@ -53,6 +53,10 @@ router.post('/forumTask', function (req, res) {
         });
 });
 
+
+/*
+* 原版：通过识别地址来区分任务产品，获取任务产品信息和价格
+* */
 router.get('/videoTask', function (req, res) {
     User.open().findById(req.session.passport.user)
         .then(function (user) {
@@ -121,6 +125,105 @@ router.get('/video/get/price', function (req, res) {
             });
         });
 });
+
+
+
+/*
+* 新版：将所有视频流量任务展开成列表，由用户自行选择添加任务
+* */
+router.get('/videoList', function (req, res) {
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            Product.open().find({
+                type: 'flow',
+                smallType: 'video'
+            }).then(function (obj) {
+                res.render('flowVideoList', {
+                    title: '视频流量任务列表',
+                    money: user.funds,
+                    username: user.username,
+                    userStatus: user.status,
+                    role: user.role,
+                    products: obj
+                })
+            });
+        });
+});
+
+router.get('/product/search/name', function (req, res) {
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            Product.open().find({
+                type: 'flow',
+                smallType: 'video',
+                name: new RegExp(req.query.productName)
+            }).then(function (obj) {
+                res.render('flowVideoList', {
+                    title: '视频流量任务列表',
+                    money: user.funds,
+                    username: user.username,
+                    userStatus: user.status,
+                    role: user.role,
+                    products: obj
+                })
+            }, function (error) {
+                res.send('查询视频流量任务列表失败： ' + error);
+            });
+        });
+
+});
+
+router.get('/add/video/task', function (req, res) {
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            Product.open().findById(req.query.id)
+                .then(function(product) {
+                    var productIns = Product.wrapToInstance(product);
+                    var price = productIns.getPriceByRole(user.role);
+                    Order.getRandomStr(req).then(function(orderFlag) {
+                        res.render('flowVideoTaskAdd', {
+                            title: '添加视频流量任务',
+                            money: user.funds,
+                            userStatus: user.status,
+                            username: user.username,
+                            role: user.role,
+                            orderFlag: orderFlag,
+                            price: price,
+                            productName: product.name
+                        });
+                    })
+                })
+
+        });
+});
+
+router.post('/videoTask/add', function (req, res) {
+    var orderInfo = req.body;
+    orderInfo.realNum = orderInfo.num;
+    if(orderInfo.num <= 10000) {
+        orderInfo.num = 10;
+    }else {
+        orderInfo.num = parseInt(orderInfo.num / 1000) + (orderInfo.num % 1000 > 0 ? 1 : 0);
+    }
+    User.open().findById(req.session.passport.user)
+        .then(function (user) {
+            var order = Order.wrapToInstance(orderInfo);
+            order.checkRandomStr(req).then(function() {
+                order.createAndSave(user, {type: 'flow', smallType: 'video', name: orderInfo.name})
+                    .then(function () {
+                        socketIO.emit('updateNav', {'flow': 1});
+                        res.redirect('/flow/taskHistory');
+                    }, function() {
+                        res.send('<h1>您的余额不足，请充值！ 顺便多说一句，请不要跳过页面非法提交数据。。。不要以为我不知道哦！！</h1>')
+                    });
+            }, function(msg) {
+                res.redirect('/flow/taskHistory');
+            })
+        });
+});
+
+
+
 
 router.get('/taskHistory', function (req, res) {
     User.open().findById(req.session.passport.user)
