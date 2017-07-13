a6cd9410070ab0be016dd78ad29c57c2fbb8fc85
zhouyong10 <1059981087@qq.com>
2017-07-13 16:03:12 +0800

    优化视屏流量推广提单

--- web-workflow/src/pages/flowForumTask/flowForumTask.js
+++ web-workflow/src/pages/flowForumTask/flowForumTask.js
@@ -1,48 +1,93 @@
 /**
  * Created by ubuntu64 on 4/18/16.
  */
-var Vue = require('vue');
-Vue.use(require('vue-validator'));
-Vue.use(require('vue-resource'));
 var Utils = require('utils');
 
-new Vue({
-    el: '#flowForum',
-    data: {
-        price: 0,
-        address: '',
-        num: '',
-        funds: 0,
-        totalPrice: 0
-    },
-    methods: {
-        judgeAddress: function() {
-            var self = this;
-            Utils.parseFlowAddress(self.$http, self.address)
-                .then(function(isFlow) {
-                    if(isFlow) {
-                        alert('检测到您提交的是视频地址，请到视频流量任务提交订单！！')
-                    }else{
-                        $('form').submit();
-                    }
-                })
+function isAddress() {
+    return new Promise(function (resolve, reject) {
+        var $address = $('#address');
+        var address = $address.val();
+        if (Utils.isHttp(address)) {
+            $address.css({color: 'green'});
+            resolve();
+            $.post('/parse/flow/forum/address', {address: address}, function (data) {
+                if(data.isFlow){
+                    alert('检测到您提交的是视频地址，请到视频流量任务提交订单！！')
+                }
+            })
+        } else {
+            $address.css({color: 'red'});
+            layer.tips('请输入正确的任务地址!', '#address', {
+                tips: [1, '#f00'],
+                time: 4000
+            });
+            reject();
         }
-    },
-    validators: {
-        isaddress: function(val) {
-            return /((http|ftp|https|file):\/\/([\w\-]+\.)+[\w\-]+(\/[\w\u4e00-\u9fa5\-\.\/?\@\%\!\&=\+\~\:\#\;\,]*)?)/ig.test(val);
-        },
-        isnum: Utils.isNum,
-        maxprice: function(input) {
-            var self = this;
-            var num;
-            if(input <= 10000) {
-                num = 10000;
-            }else {
-                num = parseInt(input / 5000) * 5000 + (input % 5000 > 0 ? 1 : 0) * 5000;
-            }
-            self.totalPrice = (num / 5000 * self.price).toFixed(4);
-            return parseFloat(self.totalPrice) <= self.funds;
+    });
+}
+
+function checkReadLike() {
+    var price = $('#price').val();
+    var userFunds = $('#userFunds').val();
+    var $readNum = $('#readNum');
+    var readNum = $readNum.val();
+    var totalPrice = 0;
+    var $total = $('#total');
+    if (Utils.isNum(readNum) && readNum > 0) {
+        $readNum.css({color: 'green'});
+        if(parseInt(readNum) <= 10000){
+            readNum = 10000;
+        }else{
+            readNum = parseInt(readNum / 5000) * 5000 + (readNum % 5000 > 0 ? 1 : 0) * 5000;
         }
+        totalPrice = (readNum / 5000 * price).toFixed(4);
+        if(parseFloat(totalPrice) > parseFloat(userFunds)){
+            $total.val('￥ ' + totalPrice).css({color: 'red'});
+            return 'errTotal';
+        }
+        $total.val('￥ ' + totalPrice).css({color: 'green'});
+        return 'ok';
+    } else {
+        $readNum.css({color: 'red'});
+        return 'errRead';
     }
+}
+
+$(function () {
+    $('#address').change(function () {
+        isAddress();
+    });
+
+    $('#readNum').keyup(function () {
+        checkReadLike();
+    }).blur(function () {
+        checkReadLike();
+    });
+
+    $('#commit').click(function (e) {
+        isAddress().then(function () {
+            var result = checkReadLike();
+            if (result != 'ok') {
+                e.stopPropagation();
+                e.preventDefault();
+                switch (result){
+                    case 'errTotal':
+                        layer.tips('账户余额不足，请充值!', '#total', {
+                            tips: [1, '#f00'],
+                            time: 4000
+                        });
+                        break;
+                    case 'errRead':
+                        layer.tips('必须为正整数!', '#readNum', {
+                            tips: [1, '#f00'],
+                            time: 4000
+                        });
+                        break;
+                }
+            }
+        }, function () {
+            e.stopPropagation();
+            e.preventDefault();
+        })
+    })
 });
--- web-workflow/src/pages/flowVideoTaskAdd/flowVideoTaskAdd.js
+++ web-workflow/src/pages/flowVideoTaskAdd/flowVideoTaskAdd.js
@@ -1,39 +1,86 @@
 /**
  * Created by ubuntu64 on 4/18/16.
  */
-var Vue = require('vue');
-Vue.use(require('vue-validator'));
-Vue.use(require('vue-resource'));
 var Utils = require('utils');
 
-new Vue({
-    el: '#flowVideo',
-    data: {
-        price: '',
-        num: '',
-        funds: 0,
-        totalPrice: 0
-    },
-    validators: {
-        isaddress: function(val) {
-            return /((http|ftp|https|file):\/\/([\w\-]+\.)+[\w\-]+(\/[\w\u4e00-\u9fa5\-\.\/?\@\%\!\&=\+\~\:\#\;\,]*)?)/ig.test(val);
-        },
-        isnum: Utils.isNum,
-        maxprice: function(input) {
-            var self = this;
-            var num;
-            if(Utils.isNum(input) && input != ''){
-                if(input <= 10000) {
-                    num = 10000;
-                }else {
-                    num = parseInt(input / 1000) * 1000 + (input % 1000 > 0 ? 1 : 0) * 1000;
-                }
-            }else {
-                num = 0;
-            }
-
-            self.totalPrice = (num / 1000 * parseFloat(self.price)).toFixed(4);
-            return parseFloat(self.totalPrice) <= parseFloat(self.funds);
-        }
+function isAddress() {
+    var $address = $('#address');
+    var address = $address.val();
+    if (Utils.isHttp(address)) {
+        $address.css({color: 'green'});
+        return true;
+    } else {
+        $address.css({color: 'red'});
+        layer.tips('请输入正确的任务地址!', '#address', {
+            tips: [1, '#f00'],
+            time: 4000
+        });
+        return false;
     }
+}
+
+function checkReadLike() {
+    var price = $('#price').val();
+    var userFunds = $('#userFunds').val();
+    var $readNum = $('#readNum');
+    var readNum = $readNum.val();
+    var totalPrice = 0;
+    var $total = $('#total');
+    if (Utils.isNum(readNum) && readNum > 0) {
+        $readNum.css({color: 'green'});
+        if(parseInt(readNum) <= 10000){
+            readNum = 10000;
+        }else{
+            readNum = parseInt(readNum / 1000) * 1000 + (readNum % 1000 > 0 ? 1 : 0) * 1000;
+        }
+        totalPrice = (readNum / 1000 * parseFloat(price)).toFixed(4);
+        if(parseFloat(totalPrice) > parseFloat(userFunds)){
+            $total.val('￥ ' + totalPrice).css({color: 'red'});
+            return 'errTotal';
+        }
+        $total.val('￥ ' + totalPrice).css({color: 'green'});
+        return 'ok';
+    } else {
+        $readNum.css({color: 'red'});
+        return 'errRead';
+    }
+}
+
+$(function () {
+    $('#address').change(function () {
+        isAddress();
+    });
+
+    $('#readNum').keyup(function () {
+        checkReadLike();
+    }).blur(function () {
+        checkReadLike();
+    });
+
+    $('#commit').click(function (e) {
+        if(isAddress()){
+            var result = checkReadLike();
+            if (result != 'ok') {
+                e.stopPropagation();
+                e.preventDefault();
+                switch (result){
+                    case 'errTotal':
+                        layer.tips('账户余额不足，请充值!', '#total', {
+                            tips: [1, '#f00'],
+                            time: 4000
+                        });
+                        break;
+                    case 'errRead':
+                        layer.tips('必须为正整数!', '#readNum', {
+                            tips: [1, '#f00'],
+                            time: 4000
+                        });
+                        break;
+                }
+            }
+        }else{
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    })
 });
