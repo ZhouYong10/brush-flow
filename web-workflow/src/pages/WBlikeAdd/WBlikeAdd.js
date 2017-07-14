83d4f9119ac46d3e7ecd8ca52afaf6aed9cba240
zhouyong10 <1059981087@qq.com>
2017-07-14 12:38:38 +0800

    优化微博点赞提单任务

--- web-workflow/src/pages/WBlikeAdd/WBlikeAdd.js
+++ web-workflow/src/pages/WBlikeAdd/WBlikeAdd.js
@@ -1,35 +1,7 @@
 /**
  * Created by ubuntu64 on 3/16/16.
  */
-var Vue = require('vue');
-Vue.use(require('vue-validator'));
-
 var Utils = require('utils');
-
-new Vue({
-    el: '#wbLike',
-    data: {
-        price: '',
-        totalPrice: 0,
-        num: '',
-        funds: 0
-    },
-    methods: {
-        total: function() {
-            this.totalPrice = (this.price * this.num).toFixed(4);
-        }
-    },
-    validators: {
-        isaddress: function(val) {
-            return /((http|ftp|https|file):\/\/([\w\-]+\.)+[\w\-]+(\/[\w\u4e00-\u9fa5\-\.\/?\@\%\!\&=\+\~\:\#\;\,]*)?)/ig.test(val);
-        },
-        isnum: Utils.isNum,
-        min100: Utils.min100,
-        maxprice: function(num) {
-            return parseFloat(this.price * num) <= parseFloat(this.funds);
-        }
-    }
-});
 
 function isAddress() {
     var $address = $('#address');
@@ -54,31 +26,15 @@ function checkReadLike() {
     var readNum = $readNum.val();
     var totalPrice = 0;
     var $total = $('#total');
-    if (Utils.isNum(readNum) && Utils.min500(readNum)) {
+    if (Utils.isNum(readNum) && Utils.min100(readNum)) {
         $readNum.css({color: 'green'});
-        if(likeNum == '') {
-            totalPrice = (readNum * price).toFixed(4);
-            if(parseFloat(totalPrice) > parseFloat(userFunds)){
-                $total.val('￥ ' + totalPrice).css({color: 'red'});
-                return 'errTotal';
-            }
-            $total.val('￥ ' + totalPrice).css({color: 'green'});
-            return 'ok';
-        }else{
-            if(Utils.isNum(likeNum) && parseInt(likeNum) <= parseInt(readNum*0.5/100)){
-                $likeNum.css({color: 'green'});
-                totalPrice = (readNum * price + likeNum * price2).toFixed(4);
-                if(parseFloat(totalPrice) > parseFloat(userFunds)){
-                    $total.val('￥ ' + totalPrice).css({color: 'red'});
-                    return 'errTotal';
-                }
-                $total.val('￥ ' + totalPrice).css({color: 'green'});
-                return 'ok';
-            }else{
-                $likeNum.css({color: 'red'});
-                return 'errLike';
-            }
+        totalPrice = (readNum * price).toFixed(4);
+        if(parseFloat(totalPrice) > parseFloat(userFunds)){
+            $total.val('￥ ' + totalPrice).css({color: 'red'});
+            return 'errTotal';
         }
+        $total.val('￥ ' + totalPrice).css({color: 'green'});
+        return 'ok';
     } else {
         $readNum.css({color: 'red'});
         return 'errRead';
@@ -86,15 +42,6 @@ function checkReadLike() {
 }
 
 $(function () {
-    $('.radioLable').click(function () {
-        var model = $(this).find('input').val();
-        $.get('/WX/like/model', {model: model}, function (data) {
-            $('#priceShow').val('阅读￥ ' + data.price + '/个, 点赞￥ ' + data.price2 + '/个');
-            $('#price').val(data.price);
-            $('#price2').val(data.price2);
-        })
-    });
-
     $('#address').change(function () {
         isAddress();
     });
@@ -105,14 +52,8 @@ $(function () {
         checkReadLike();
     });
 
-    $('#likeNum').keyup(function () {
-        checkReadLike();
-    }).blur(function () {
-        checkReadLike();
-    });
-
     $('#commit').click(function (e) {
-        isAddress().then(function () {
+        if(isAddress()){
             var result = checkReadLike();
             if (result != 'ok') {
                 e.stopPropagation();
@@ -124,23 +65,17 @@ $(function () {
                             time: 4000
                         });
                         break;
-                    case 'errLike':
-                        layer.tips('点赞数必须是正整数，且不能大于阅读数的0.5%!', '#likeNum', {
-                            tips: [1, '#f00'],
-                            time: 4000
-                        });
-                        break;
                     case 'errRead':
-                        layer.tips('阅读数必须是正整数，且不小于500!', '#readNum', {
+                        layer.tips('必须是正整数，且不小于100!', '#readNum', {
                             tips: [1, '#f00'],
                             time: 4000
                         });
                         break;
                 }
             }
-        }, function () {
+        }else{
             e.stopPropagation();
             e.preventDefault();
-        })
+        }
     })
 });
