965c34c633561e0aef3e8906d62fc12525f7c288
zhouyong10 <1059981087@qq.com>
2017-07-14 15:29:51 +0800

    优化提现

--- web-workflow/src/pages/withdrawAdd/withdrawAdd.js
+++ web-workflow/src/pages/withdrawAdd/withdrawAdd.js
@@ -1,23 +1,81 @@
 /**
  * Created by ubuntu64 on 3/21/16.
  */
-var Vue = require('vue');
-Vue.use(require('vue-validator'));
 var Utils = require('utils');
 
-new Vue({
-    el: '#withdraw',
-    data: {
-        userFunds: ''
-    },
-    validators: {
-        isfloat: Utils.isfloat,
-        mustOne: function(funds) {
-            return parseFloat(funds) >= 1;
-        },
-        mintotal: function(funds) {
-            var val = funds.trim();
-            return val == '' ? true : parseFloat(val) <= parseFloat(this.userFunds);
+function checkFunds() {
+    var userFunds = $('#userFunds').val();
+    var $funds = $('#funds');
+    var funds = $funds.val();
+    console.log(funds, '----------------');
+    console.log(userFunds, '----------------');
+    if(Utils.isfloat(funds) && funds >=1) {
+        if(parseFloat(funds) > parseFloat(userFunds)){
+            $funds.css({color: 'red'});
+            layer.tips('账户余额不足', '#funds', {
+                tips: [1, '#f00'],
+                time: 4000
+            });
+            return false;
         }
+        $funds.css({color: 'green'});
+        return true;
+    }else{
+        $funds.css({color: 'red'});
+        layer.tips('请输入合法金额，且不能小于1', '#funds', {
+            tips: [1, '#f00'],
+            time: 4000
+        });
+        return false;
     }
+}
+
+function checkCount() {
+    var $aliCount = $('#aliCount');
+    var aliCount = $aliCount.val().trim();
+    if(aliCount == ''){
+        layer.tips('请输入支付宝账户', '#aliCount', {
+            tips: [1, '#f00'],
+            time: 4000
+        });
+    }else{
+        $aliCount.css({color: 'green'});
+        return true;
+    }
+}
+
+function checkName() {
+    var $aliName = $('#aliName');
+    var aliName = $aliName.val().trim();
+    if(aliName == ''){
+        layer.tips('请输入支付宝昵称', '#aliName', {
+            tips: [1, '#f00'],
+            time: 4000
+        });
+    }else{
+        $aliName.css({color: 'green'});
+        return true;
+    }
+}
+
+$(function () {
+
+    $('#funds').blur(function () {
+        checkFunds();
+    });
+
+    $('#aliCount').blur(function () {
+        checkCount();
+    });
+
+    $('#aliName').blur(function () {
+        checkName();
+    });
+
+    $('#commit').click(function (e) {
+        if (!(checkFunds() && checkCount() &&  checkName())) {
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    })
 });
