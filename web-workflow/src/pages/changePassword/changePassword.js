426b68415a71775e6336ee53fc0123ca02677547
zhouyong10 <1059981087@qq.com>
2017-07-14 16:18:39 +0800

    优化密码修改

--- web-workflow/src/pages/changePassword/changePassword.js
+++ web-workflow/src/pages/changePassword/changePassword.js
@@ -1,40 +1,78 @@
 /**
  * Created by zhouyong10 on 2/18/16.
  */
-var Vue = require('vue');
-Vue.use(require('vue-validator'));
-Vue.use(require('vue-resource'));
-
-new Vue({
-    el: '#changePassword',
-    data: {
-        oldpwd: '',
-        newpwd: '',
-        repeatpwd: ''
-    },
-    validators: {
-        same: function(pwd) {
-            return pwd === this.newpwd;
-        }
-    },
-    methods: {
-        submit: function() {
-            var username = $('#username').val();
-            if(username == 'yanshi'){
-                layer.msg('演示账户不能不能修改密码！')
-            }else {
-                this.$http.post('/user/changePwd', {
-                    oldpwd: this.oldpwd,
-                    newpwd: this.newpwd,
-                    repeatpwd: this.repeatpwd
-                }).then(function(res) {
-                    if(res.data.isOK) {
-                        $('<a href="' + res.data.url + '" ></a>').get(0).click();
-                    }else{
-                        layer.msg(res.data.info);
-                    }
-                });
-            }
-        }
+function oldPwd() {
+    var $oldPwd = $('#oldPwd');
+    var oldPwd = $oldPwd.val().trim();
+    if(oldPwd == ''){
+        layer.tips('不能为空！', '#oldPwd', {
+            tips: [1, '#f00'],
+            time: 4000
+        });
+        return false;
+    }else{
+        return true;
     }
+}
+
+function newPwd() {
+    var $newPwd = $('#newPwd');
+    var newPwd = $newPwd.val().trim();
+    if(newPwd == ''){
+        layer.tips('不能为空！', '#newPwd', {
+            tips: [1, '#f00'],
+            time: 4000
+        });
+        return false;
+    }else{
+        return true;
+    }
+}
+
+function rNewPwd() {
+    var newPwd = $('#newPwd').val().trim();
+    var $rNewPwd = $('#rNewPwd');
+    var rNewPwd = $rNewPwd.val().trim();
+    if(newPwd != rNewPwd){
+        layer.tips('两次输入的密码不一致！', '#rNewPwd', {
+            tips: [1, '#f00'],
+            time: 4000
+        });
+        return false;
+    }else{
+        return true;
+    }
+}
+
+
+$(function () {
+    $('#oldPwd').blur(function () {
+        oldPwd();
+    });
+
+    $('#newPwd').blur(function () {
+        newPwd();
+    });
+
+    $('#rNewPwd').blur(function () {
+        rNewPwd();
+    });
+
+    $('#commit').click(function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        if(oldPwd() && newPwd() && rNewPwd()){
+            $.post('/user/changePwd', {
+                oldpwd: $('#oldPwd').val(),
+                newpwd: $('#newPwd').val(),
+                repeatpwd: $('#rNewPwd').val()
+            }).then(function(data) {
+                if(data.isOK) {
+                    $('<a href="' + data.url + '" ></a>').get(0).click();
+                }else{
+                    layer.msg(data.info);
+                }
+            });
+        }
+    })
 });
