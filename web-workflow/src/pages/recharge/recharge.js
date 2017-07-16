75a8340f123e943c45e3ec4e6f90498e31b73b60
zhouyong10 <1059981087@qq.com>
2017-07-16 09:41:09 +0800

    优化充值

--- web-workflow/src/pages/recharge/recharge.js
+++ web-workflow/src/pages/recharge/recharge.js
@@ -31,3 +31,30 @@ new Vue({
         isnum: Utils.isNum
     }
 });
+
+$(function() {
+    $('#commit').click(function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        var $alipayNum = $('#rechargeNum');
+        var alipayNum = $alipayNum.val();
+        var numLen = alipayNum.length;
+        if(Utils.isNum(alipayNum) && numLen ==32){
+            $.post('/user/recharge', {
+                alipayId: alipayNum
+            }).then(function(data) {
+                console.log(data, '====================');
+                if(data.isOK) {
+                    $('<a href="' + data.path + '" ></a>').get(0).click();
+                }else{
+                    layer.msg(data.message);
+                }
+            });
+        }else{
+            layer.tips('请输入合法的支付宝交易号!', '#rechargeNum', {
+                tips: [1, '#f00'],
+                time: 4000
+            });
+        }
+    })
+})
