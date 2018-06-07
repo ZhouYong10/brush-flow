d1f75172cd381086de71b0db098c23b96a22ed48
zhouyong10 <1059981087@qq.com>
2018-06-07 11:38:41 +0800

    添加自启动抓取微信文章标题的服务的脚本

--- backstage/bin/phantomjs-server.js
+++ backstage/bin/phantomjs-server.js
@@ -6,25 +6,21 @@
  */
 var webserver = require('webserver').create();
 var page = require('webpage').create();
-var system = require('system');
 
 var port = 8899;
 
-webserver.listen(port, function(req, res) {
+webserver.listen(port, function (req, res) {
     var url = req.url.split('=')[1];
-    page.open(url, function(status) {
-        if(status == 'success'){
+    page.open(url, function (status) {
+        if (status == 'success') {
             var title = page.evaluate(function () {
                 return document.getElementById('activity-name').innerText;
             });
             res.write(title);
             res.close();
-        }else{
+        } else {
             res.write('get title failed');
             res.close();
         }
     })
-
-    page.onError = function () {
-    };
-})
+});
---
+++ backstage/bin/start-phantomjs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#  nohup ./start-phantomjs.sh &
+
+while true
+do
+stillRunning=$(ps -ef |grep "phantomjs --ignore-ssl-errors=true --ssl-protocol=any phantomjs-server.js" |grep -v "grep")
+if [ "$stillRunning" ] ; then
+echo "phantomjs service was already started."
+else
+echo "phantomjs service was not started"
+echo "Starting service ..."
+nohup phantomjs --ignore-ssl-errors=true --ssl-protocol=any phantomjs-server.js &
+echo "phantomjs service was exited!"
+fi
+sleep 10
+done
