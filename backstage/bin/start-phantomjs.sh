e37a85b938a419cfd2065c1f3e9e148399d4b8a7
zhouyong10 <1059981087@qq.com>
2018-09-17 15:30:19 +0800

    恢复获取标题进程启动方式

--- backstage/bin/start-phantomjs.sh
+++ backstage/bin/start-phantomjs.sh
@@ -2,23 +2,16 @@
 
 #  nohup ./start-phantomjs.sh &
 
-## while true
-## do
-## stillRunning=$(ps -ef |grep "phantomjs --ignore-ssl-errors=true --ssl-protocol=any phantomjs-server.js" |grep -v "grep")
-## if [ "$stillRunning" ] ; then
-## echo "phantomjs service was already started."
-## else
-## echo "phantomjs service was not started"
-## echo "Starting service ..."
-## nohup phantomjs --ignore-ssl-errors=true --ssl-protocol=any phantomjs-server.js &
-## echo "phantomjs service was exited!"
-## fi
-## sleep 10
-## done
-
-
 while true
 do
+stillRunning=$(ps -ef |grep "phantomjs --ignore-ssl-errors=true --ssl-protocol=any phantomjs-server.js" |grep -v "grep")
+if [ "$stillRunning" ] ; then
+echo "phantomjs service was already started."
+else
+echo "phantomjs service was not started"
+echo "Starting service ..."
 nohup phantomjs --ignore-ssl-errors=true --ssl-protocol=any phantomjs-server.js &
-sleep 60 * 60 * 2
+echo "phantomjs service was exited!"
+fi
+sleep 10
 done
